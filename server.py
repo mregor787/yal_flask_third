3d4cc8753feca8e78c9615909358aba914f5f0ac
Egor Belousov <egor0288354@yandex.ru>
2021-03-11 02:07:31 +0300

    Task #2 (Carousel)

--- server.py
+++ server.py
@@ -38,5 +38,38 @@ def photo():
         return redirect('/load_photo')
 
 
+@app.route('/carousel')
+def carousel():
+    return f'''
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" 
+                    rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" 
+                    crossorigin="anonymous">
+                    <title>Пейзажи Марса</title>
+                </head>
+                <body>
+                    <h1 style="text-align:center;">Пейзажи Марса</h1>
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="max-width: 1000px; margin: auto;">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{url_for('static', filename='img/mars/city.jpg')}" alt="First slide" style="width: 100%;">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="{url_for('static', filename='img/mars/mars.jpg')}" alt="Second slide" style="width: 100%;">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="{url_for('static', filename='img/mars/red_planet.jpg')}" alt="Third slide" style="width: 100%;">
+                            </div>
+                        </div>
+                    </div>
+                </body>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+            </html>
+            '''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
